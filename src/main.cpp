e2b6b42be38bdfa38c03cd1daeff4b7aeeb1c8d9
NotKieranF <89042089+NotKieranF@users.noreply.github.com>
2025-08-12 20:05:39 -0400

    Began work on NES transmission

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,40 @@
-#include "avr/interrupt.h"
-#include "avr/io.h"
 #include "include/gamecube_to_nes.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+/*
+ * Map:
+ *  PA0 - SPI0 /SS - Should be tied low
+ *  PA1 - SPI0 MOSI - Connected to NES_OUT, triggers an interrupt on falling edge
+ *  PA2 - SPI0 MISO - Connected to NES_D0
+ *  PA3 - SPI0 SCK - Connected to NES_CLK, clocks shift register on rising edge
+ *  PA7 - TCA0 WO0 - Connected to GC_DATA via a 5V-3.3V level shifter
+ */
+
+/*
+ *  PA0 - TCA0 WO0
+ *  PE0 - SPI0 MOSI
+ *  PE1 - SPI0 MISO
+ *  PE2 - SPI0 SCK
+ *  PE3 - SPI0 /SS
+ */
+#define SPI0_MOSI_PIN (0)
+#define SPI0_MISO_PIN (1)
+#define SPI0_SCK_PIN (2)
+#define SPI0_SS_PIN (3)
+
+nesInputPacket inputBufferA;
+nesInputPacket inputBufferB;
+nesInputPacket inputBufferC;
+nesInputPacket *frontBuffer;
+nesInputPacket *backBuffer;
+nesInputPacket *nextBuffer;
 
 /*
  * Gamecube serial transmissions are encoded using the TCA0 peripheral using
- * the double-buffered single-shot PWM mode. An interrupt is triggered on timer
- * overflow, and a new compare threshold must be written within one period
+ * the double-buffered single-shot PWM mode on compare channel 0. An interrupt
+ * is triggered on timer overflow, and a new compare threshold must be written
+ * within one period (5us)
  */
 unsigned char gamecubeTxState = 0;
 unsigned char gamecubeTxLength = 4;
@@ -17,7 +46,7 @@ char *gamecubeTxPtr = gamecubeTxBufferA;
 /*
  * Gamecube serial transmissions are decoded using the TCB0 peripheral using the
  * pulse-width capture mode. An interrupt is triggered on capture end, and the
- * result must be read and serialized
+ * result must be read and serialized within one period (4us)
  */
 unsigned char gamecubeRxLength = 0;
 char gamecubeRxShiftRegister = 0x80;
@@ -27,95 +56,143 @@ char *gamecubeRxPtr = gamecubeRxBufferA;
 
 /*
  * NES serial transmissions are encoded using the SPI0 peripheral in buffered
- * slave mode.
+ * slave mode. ...10 cycles 
  */
-unsigned char nesTxLength;
-char *nesTxPtr;
-char nesTxBufferA[5];
+unsigned char nesTxLength = 5;
+char nesTxBufferA[5] = {0x00, 0x01, 0x02, 0x03};
 char nesTxBufferB[5];
-
-// Gamecube command packets
-void transmitGamecubePacket() {
-  // Set GCDATA pin to be output
-  // Prime TCA.CMP0 with first bit
-  // Buffer second bit into TCA.CMP0BUF
-  // Prime output to be high
-  //  TCA0.SINGLE.CTRLC = TCA_SINGLE_CMP0OV_bm;
-  // Enable LUT1
-  // Enable TCA
-}
+char *nesTxPtr = nesTxBufferA;
 
 // Gamecube transmissions are handled by TCA and CCL peripherals, and output on
 // pin PC6
 void initGamecubeTx() {
-  // Perform a hard reset on the TCA0 unit, switch to normal mode
-  TCA0.SINGLE.CTRLA = 0;
-  TCA0.SINGLE.CTRLESET = TCA_SINGLE_CMD_RESET_gc;
-  TCA0.SINGLE.CTRLD = 0;
+    // Perform a hard reset on the TCA0 unit, switch to normal mode
+    TCA0.SINGLE.CTRLA = 0;
+    TCA0.SINGLE.CTRLESET = TCA_SINGLE_CMD_RESET_gc;
+    TCA0.SINGLE.CTRLD = 0;
 
-  // TCA unit is directly driven by the peripheral clock, no prescaling, and no
-  // event inputs
-  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc;
-  TCA0.SINGLE.EVCTRL = 0;
-  // Gamecube controllers expect a new bit every 5us
-  TCA0.SINGLE.PER = GC_PWM_PERIOD;
-  // Trigger interrupt on overflow, so we can buffer a new bit into CMP0
-  TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
+    // TCA unit is directly driven by the peripheral clock, no prescaling, and no
+    // event inputs
+    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc;
+    TCA0.SINGLE.EVCTRL = 0;
+    // Gamecube controllers expect a new bit every 5us
+    TCA0.SINGLE.PER = GC_PWM_TX_PERIOD;
+    // Trigger interrupt on overflow, so we can buffer a new bit into CMP0
+    TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
 
-  // Use single slope PWM mode on compare channel 0 to generate waveform
-  TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc;
-  // Notably, the output of compare channel 0 in normal mode cannot be mapped
-  // directly to a GPIO pin on the attiny212, so we must pipe it through the CCL
-  // unit first, specifically LUT1
-  CCL.SEQCTRL0 = CCL_SEQSEL1_DISABLE_gc;
-  CCL.LUT1CTRLB = CCL_INSEL0_TCA0_gc | CCL_INSEL1_MASK_gc;
-  CCL.LUT1CTRLC = CCL_INSEL2_MASK_gc;
-  CCL.TRUTH1 = 0b10101010;
-  CCL.LUT1CTRLA = CCL_FILTSEL_DISABLE_gc | CCL_OUTEN_bm;
+    // Use single slope PWM mode on compare channel 0 to generate waveform
+    TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc;
+    // Notably, the output of compare channel 0 in normal mode cannot be mapped
+    // directly to a GPIO pin on the attiny212, so we must pipe it through the CCL
+    // unit first, specifically LUT1
+    CCL.SEQCTRL0 = CCL_SEQSEL1_DISABLE_gc;
+    CCL.LUT1CTRLB = CCL_INSEL0_TCA0_gc | CCL_INSEL1_MASK_gc;
+    CCL.LUT1CTRLC = CCL_INSEL2_MASK_gc;
+    CCL.TRUTH1 = 0b10101010;
+    CCL.LUT1CTRLA = CCL_FILTSEL_DISABLE_gc | CCL_OUTEN_bm;
 
-  // LUT must be enabled separately, after all other initialization
-  // (This should also be done separately)
-  CCL.LUT1CTRLA |= CCL_ENABLE_bm;
-  CCL.CTRLA = CCL_ENABLE_bm;
+    // LUT must be enabled separately, after all other initialization
+    // (This should also be done separately)
+    CCL.LUT1CTRLA |= CCL_ENABLE_bm;
+    CCL.CTRLA = CCL_ENABLE_bm;
 
-  // Invert direction??
-  PORTC.PIN6CTRL = 0b10000000 | PORT_ISC_INTDISABLE_gc;
-  PORTC.DIRCLR = 0b01000000;
+    // Invert direction??
+    PORTC.PIN6CTRL = 0b10000000 | PORT_ISC_INTDISABLE_gc;
+    PORTC.DIRCLR = 0b01000000;
 
-  // This is a temporary thing, we don't really need to initialize the pwm here
-  TCA0.SINGLE.CMP0 = 0;
-  TCA0.SINGLE.CMP0BUF = 0;
-  gamecubePWMBuffer = 0;
-  TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
-  PORTMUX.CCLROUTEA = PORTMUX_LUT1_bm;
+    // This is a temporary thing, we don't really need to initialize the pwm here
+    TCA0.SINGLE.CMP0BUF = 0;
+    TCA0.SINGLE.CMP0 = 0;
+    gamecubePWMBuffer = 0;
+    TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
+    PORTMUX.CCLROUTEA = PORTMUX_LUT1_bm;
+}
+
+//
+void pollGamecubeController() {
+    // Initialize TCA0 buffer chain to be initially high
+    TCA0.SINGLE.CMP0BUF = 0;
+    TCA0.SINGLE.CMP0 = 0;
+    gamecubePWMBuffer = 0;
+
+    // Enabling LUT1 overrides pin direction and begins output
+    CCL.LUT1CTRLA |= CCL_ENABLE_bm;
+    CCL.CTRLA = CCL_ENABLE_bm;
+
+    // Enabling TCA0 begins command transmission
+    TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
+
+        // Wait for controller response
+        while (1);
 }
 
 int main() {
-  // Disable clock prescaling so we can run at the full 20MHz
-  CCP = 0xd8;
-  CLKCTRL.MCLKCTRLB = 0;
-  asm volatile("sei" ::: "memory");
+    // Disable clock prescaling so we can run at the full 20MHz
+    CCP = 0xd8;
+    CLKCTRL.MCLKCTRLB = 0;
 
-  //
-  TCB0.CTRLB = TCB_CNTMODE_PW_gc;
-  TCB0.EVCTRL = 0b01010001;
-  TCB0.INTCTRL = 0b00000001;
-  TCB0.CTRLA = /*TCB_ENABLE_bm |*/ TCB_CLKSEL_CLKDIV1_gc;
-  EVSYS.CHANNEL2 = EVSYS_GENERATOR_PORT0_PIN6_gc;
-  EVSYS.USERTCB0 = EVSYS_CHANNEL_CHANNEL2_gc;
+    // Initialize TCA0
 
-  initGamecubeTx();
+    // Initialize TCB0
 
-  PORTB.DIRSET = 0b00000100;
-  PORTB.OUTSET = 0b00000100;
+    // Initialize SPI0
+    // Place SPI interface on pins PE[3:0]
+    PORTMUX.TWISPIROUTEA = (PORTMUX.TWISPIROUTEA & ~PORTMUX_SPI0_gm) | PORTMUX_SPI0_ALT2_gc;
 
-  while (1) {
-    /*
-     * Steps per iteration:
-     *  1) Initiate controller poll
-     *  2) Wait for response to be written to rx buffer
-     *  3) Copy response to tx buffer
-     *  4) Loop?
-     */
-  }
+    //
+    SPI0.CTRLB = SPI_BUFEN_bm | SPI_MODE_3_gc;
+    SPI0.INTCTRL = SPI_TXCIE_bm;
+    SPI0.CTRLA = 0;
+
+    PORTE.DIRSET = 0b00000010;
+    PORTE.DIRCLR = 0b00001101;
+    PORTE.PIN0CTRL = PORT_PULLUPEN_bm | PORT_ISC_RISING_gc;
+    PORTE.PIN2CTRL = PORT_PULLUPEN_bm;
+    PORTE.PIN3CTRL = PORT_PULLUPEN_bm;
+
+    // Enable interrupts post-init
+    asm volatile("sei" ::: "memory");
+
+    //
+//    TCB0.CTRLB = TCB_CNTMODE_PW_gc;
+//    TCB0.EVCTRL = 0b01010001;
+//    TCB0.INTCTRL = 0b00000001;
+//    TCB0.CTRLA = /*TCB_ENABLE_bm |*/ TCB_CLKSEL_CLKDIV1_gc;
+//    EVSYS.CHANNEL2 = EVSYS_GENERATOR_PORT0_PIN6_gc;
+//    EVSYS.USERTCB0 = EVSYS_CHANNEL_CHANNEL2_gc;
+//
+//    initGamecubeTx();
+//    TCA0.SINGLE.CMP2BUF = GC_PWM_RX_TIMEOUT;
+//    TCA0.SINGLE.CMP2 = GC_PWM_RX_TIMEOUT;
+//
+    PORTB.DIRSET = 0b00000010;
+    PORTB.OUTSET = 0b00000010;
+
+    while (1) {
+        /*
+         * Steps per iteration:
+         *  1) Initiate controller poll
+         *  2) Wait for response to be written to rx buffer
+         *  3) Process response
+         *  4) Copy response to tx buffer
+         *  5) Loop?
+         */
+    }
 }
+
+//uint8_t counter = 0;
+//
+//ISR(PORTE_PORT_vect, ISR_BLOCK) {
+//  PORTE.INTFLAGS = 0b00000001;
+//  counter++;
+//  // SPI0 normal mode
+//  // Put first byte into shift register
+//  // SPI0 buffered mode
+//  // Put second byte into buffer
+//}
+//
+//ISR(SPI0_INT_vect, ISR_BLOCK) {
+//  SPI0.DATA = counter;
+//  SPI0.INTFLAGS = SPI_TXCIF_bm;
+//  PORTB.OUTTGL = _BV(1);
+//}
